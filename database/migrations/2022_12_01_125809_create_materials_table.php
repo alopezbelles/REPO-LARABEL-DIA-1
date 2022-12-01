de6a15ca75c678cc8cdb11d1dcebc8b0e32399e9
alopezbelles <alopezbelles@gmail.com>
2022-12-01 13:59:10 +0100

    add materials table

--- database/migrations/2022_12_01_125809_create_materials_table.php
+++ database/migrations/2022_12_01_125809_create_materials_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::table('tasks', function (Blueprint $table) {
-           $table->boolean('status')->after('name');
-
+        Schema::create('materials', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
         });
     }
 
@@ -26,7 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::table('tasks', function (Blueprint $table) {
-        });
+        Schema::dropIfExists('materials');
     }
 };
