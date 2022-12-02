09a37cf54d1d21ec4f7658fca32f9ab950d234f5
alopezbelles <alopezbelles@gmail.com>
2022-12-02 13:58:00 +0100

    feat: add param url id task id

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,6 +23,6 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             TaskSeeder::class,
-        ])
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/welcome', function () {
+    return 'Welcome to my tasks app';
+});
+
+// 4 nuevos, get post put delete, con la ruta correspondiente a tareas. Nos tiene que devolver (return). Esto es ....put tareas, etc. 
+
+Route::put('/put', function ($id) {
+    return 'update task: '.$id;
+});
+
+Route::post('/welcome', function () {
+    return 'Esto es un post............';
+});
+
+
+Route::delete('/delete', function ($id) {
+    return 'delete task: '.$id;
+});
+
+Route::get('/welcome', function () {
+    return 'Welcome to my tasks app';
 });
